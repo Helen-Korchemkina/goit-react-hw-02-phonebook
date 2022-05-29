47e3645add521c7d2055b1a6146d953f7cf0783b
Helen <dd040388kev@gmail.com>
2022-05-29 19:24:12 +0300

    final

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,81 +1,78 @@
-import React, { Component } from "react";
-import { nanoid } from "nanoid";
-import ContactForm from "components/ContactForm/ContactForm";
-import Filter from "components/Filter/Filter";
-import ContactList from "components/ContactList/ContactList";
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from 'components/ContactForm/ContactForm';
+import Filter from 'components/Filter/Filter';
+import ContactList from 'components/ContactList/ContactList';
 import s from './App.module.css';
 
 class App extends Component {
-state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
-};
-    
-  addContact = data => {
-    const contact = {
-      id: nanoid(),
-      name: data.name,
-      number: data.number,
-    };
+  };
 
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
-  }
-  
+  addContact = ({ name, number }) => {
+    this.setState(({ contacts }) => {
+      const isContact = contacts.find(contact => contact.name === name);
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      if (isContact) {
+        alert(`${name} is already in contact`);
+        return contacts;
+      } else {
+        return {
+          contacts: [newContact, ...contacts],
+        };
+      }
+    });
+  };
+
   deleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
 
-  //   toggleCompleted = contactId => {
-  //   this.setState(({ contacts }) => ({
-  //     contacts: contacts.map(contact =>
-  //       contact.id === contactId ? { ...contact, completed: !contact.completed } : contact,
-  //     ),
-  //   }));
-  // };
-
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
-  }
+  };
 
-  //   getVisibleContact = () => {
-  //   const { filter, contacts } = this.state;
-  //   const normalizedFilter = filter.toLowerCase();
+  getFiltredContacts = () => {
+    const normalizedFilter = this.state.filter.toLowerCase();
 
-  //   return contacts.filter(contact =>
-  //     contact.name.text.toLowerCase().includes(normalizedFilter),
-  //   );
-  // };
+    return this.state.contacts
+      .map(
+        contact =>
+          contact.name.toLowerCase().includes(normalizedFilter) && contact
+      )
+      .filter(contact => contact !== false);
+  };
 
   render() {
-  
+    const filtredContacts = this.getFiltredContacts();
+
     return (
       <div className={s.form}>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.addContact} />
         <h2>Contacts</h2>
-        <Filter
-          value={this.state.filter}
-          onChange={this.changeFilter}
-        />
-        <ContactList 
-          contacts={this.state.contacts}
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={filtredContacts}
           onDeleteContact={this.deleteContact}
-          // onToggleCompleted={this.toggleCompleted}
         />
       </div>
-    )
+    );
+  }
 }
-};
 
 export default App;
-
-
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
-    <ul>
-{contacts.map(({ id, name, number }) => (
-            <li key={id}><p>{name}</p><p>{number}</p>
-            <button type="button" onClick={() => onDeleteContact(id)}>Delete</button>
-            </li>    
-        ))}
-        </ul>
+  <ul className={s.list}>
+    {contacts.map(({ id, name, number }) => (
+      <li className={s.contact} key={id}>
+        <p>{name}:</p>
+        <p>{number}</p>
+        <button
+          className={s.btn}
+          type="button"
+          onClick={() => onDeleteContact(id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
 );
 
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired)),
+  onDeleteContact: PropTypes.func.isRequired,
+};
 
 export default ContactList;
